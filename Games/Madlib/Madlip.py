37feab8f1ba80e8b89302e874d93846f438f1d86
PedroMaluf <53538736+PedroMaluf@users.noreply.github.com>
2022-05-30 13:32:12 -0300

    Delete Games directory

--- Games/Madlib/Madlip.py
+++
@@ -1,7 +0,0 @@
-from Amostras_madlibs import Code, Zombies, Zoo
-import random
-
-#Call randomly one of the 3 madlibs
-if __name__ == "__main__":
-    m = random.choice([Code, Zombies, Zoo])
-    m.madlib()
