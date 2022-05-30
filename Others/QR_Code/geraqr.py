9d5f7dde6949fea629c93f0de0b19c5b7a44241a
PedroMaluf <53538736+PedroMaluf@users.noreply.github.com>
2022-05-30 13:32:43 -0300

    Delete Others directory

--- Others/QR_Code/geraqr.py
+++
@@ -1,3 +0,0 @@
-import qrcode
-
-qrcode.make('teste teste').save('C:/Projetospy/Qrimagens/qrteste.png')
--- Others/QR_Code/leqr.py
+++
@@ -1,4 +0,0 @@
-from pyzbar.pyzbar import decode
-from PIL import Image
-
-print(decode(Image.open('C:/Projetospy/Qrimagens/leitura.png')))
